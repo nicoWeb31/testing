0c9423e32d4467297aa858c62e2a6749162fc957
nicoWeb31 <nico.riot@free.fr>
2021-01-12 19:55:27 +0100

    enzimz

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,25 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import App from "../../components/App";
 import ReactDOM from "react-dom";
+import '@testing-library/jest-dom/extend-expect';
 
 test("shows a comment box", () => {
     // render(<App />);
-    // const linkElement = screen.getByText(/the/i);
+    // // const linkElement = screen.getByText(/the/i);
+    // const comment = screen.getByText('Comment Box')
+    
+    // expect(comment).toBeInTheDocument()
+
+
+
+
+    // const div = document.createElement("div");
+    // ReactDOM.render(<App />, div);
+    // //look inside the div
+    // //and check to see if the component box is in there
+
+    // console.log(div.innerHTML);
+    // expect(div.innerHTML).toContain('Comment Box')
+
+    // ReactDOM.unmountComponentAtNode(div);
 
     
-    // expect(linkElement).toBeInTheDocument()
 
-
-
-
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
-    //look inside the div
-    //and check to see if the component box is in there
-
-    console.log(div.innerHTML);
-    expect(div.innerHTML).toContain('Comment Box')
-
-    ReactDOM.unmountComponentAtNode(div);
 
 });
