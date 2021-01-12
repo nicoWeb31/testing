cb2a1db49a2fe5023956237effd5af83144b98f0
nicoWeb31 <nico.riot@free.fr>
2021-01-12 15:01:04 +0100

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hi there/i);
   expect(linkElement).toBeInTheDocument();
+
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // expect(div.innerHTML).toContain('hi there');
+
+  // ReactDOM.unmountComponentAtNode(div);
 });
