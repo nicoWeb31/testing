49002f1e5fc1864f047c3ab3be7cbf57ff0cc72a
nicoWeb31 <nico.riot@free.fr>
2021-01-13 16:40:21 +0100

    routine redux

---
+++ src/components/redux/reducer/commentReducer.js
@@ -0,0 +1,11 @@
+
+
+exports.commentReducer = (state=[], action) =>{
+switch(action.type){
+
+
+    default :
+        return state;
+
+}
+}
---
+++ src/components/redux/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { commentReducer } from 'redux/reducer/commentReducer';
+
+
+
+export default combineReducers({
+    comments: commentReducer,
+})
