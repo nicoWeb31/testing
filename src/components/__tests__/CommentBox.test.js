43c234a3dc6b6f4f9acb139678c5e4284930d4c1
nicoWeb31 <nico.riot@free.fr>
2021-01-13 15:31:03 +0100

    test form fin

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,32 +1,53 @@
-import React from 'react';
+import React from "react";
 import CommentBox from "components/CommentBox";
 
 import { mount } from "enzyme";
 
 let wrapped;
-beforeEach(()=>{
+beforeEach(() => {
     wrapped = mount(<CommentBox />);
-})
-afterEach(()=>{
-    wrapped.unmount()
-})
-
-
-
-test('has a textarea and button', ()=>{
-
-// console.log(wrapped.find('textarea').length);
-// console.log(wrapped.find('button').length);
-expect(wrapped.find('textarea').length).toBe(1);
-expect(wrapped.find('button').length).toEqual(1);
-
-
+});
+afterEach(() => {
+    wrapped.unmount();
 });
 
 
-test('has a text area thats unser type in', ()=>{
 
-    wrapped.find('textarea').simulate('change', {
-        target: {value: 'new comment'}
+
+test("has a textarea and button", () => {
+    // console.log(wrapped.find('textarea').length);
+    // console.log(wrapped.find('button').length);
+    expect(wrapped.find("textarea").length).toBe(1);
+    expect(wrapped.find("button").length).toEqual(1);
+});
+
+
+describe('the textarea', () => {
+
+    beforeEach(() => {
+        wrapped
+        .find("textarea")
+        .simulate("change", {
+            target: { value: "new comment" },
+        })
+        .update();
     })
+
+    test("has a text area thats unser type in", () => {
+        
+    
+        expect(wrapped.find("textarea").prop("value")).toBe("new comment");
+    });
+    
+    test("has onSubmit by user, textarea is empty", () => {
+        // wrapped
+        //     .find("textarea")
+        //     .simulate("change", { target: { value: "" } })
+        //     .update();
+        wrapped.find("form").simulate("submit").update();
+    
+        expect(wrapped.find("textarea").prop("value")).toBe("");
+    });
+
 })
+
