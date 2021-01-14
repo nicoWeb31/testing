7f1a674cbe7af531c086e0836f63712be43d0f90
nicoWeb31 <nico.riot@free.fr>
2021-01-14 16:37:08 +0100

    action fetch comment

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -20,3 +20,12 @@ beforeEach(() => {
 test("creates on LI per comment", () => {
     expect(wrapped.find('li').length).toEqual(3);
 });
+
+
+test('show the text for each comment', () => {
+    // console.log(wrapped.render().text())
+    expect(wrapped.render().text()).toContain('comment3')
+    expect(wrapped.render().text()).toContain('dlfkdldfkdmf')
+    expect(wrapped.render().text()).toContain('skdfj')
+
+})
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,13 @@
-import {SAVE_COMMENT } from 'redux/type/type.js';
+import { SAVE_COMMENT, FETCH_COMMENTS } from "redux/type/type.js";
+import axios from "axios";
 
-export const saveComment = (comment) =>{
+export const saveComment = (comment) => {
+    return { type: SAVE_COMMENT, payload: comment };
+};
 
-    return {type: SAVE_COMMENT, payload: comment};
-
-}
+export const getComment = () => async (dispatch) => {
+    const data = await axios.get(
+        "http://jsonplaceholder.typicode.com/comments"
+    );
+    dispatch({ type: FETCH_COMMENTS, payload: data });
+};
